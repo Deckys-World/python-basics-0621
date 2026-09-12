c7de77b7f62cec8f1de531caf217977358b60872
Deckys-World <82792834+Deckys-World@users.noreply.github.com>
2026-09-12 08:56:33 +0530

    create : factorial.py

---
+++ Factorial.py
@@ -0,0 +1,13 @@
+n = int(input("Enter the number of terms: "))
+
+first = 0
+second = 1
+
+print("Fibonacci Series:", end=" ")
+
+for i in range(n):
+    print(first, end=" ")
+
+    next_term = first + second
+    first = second
+    second = next_term
